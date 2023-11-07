68e1179edb23ef2743a43f6040fcd37b1adf1a83
Andrew Bonner <andrew.bonner@bsu.edu>
2023-11-07 13:20:18 -0500

    Change the pubspec.yaml dependencies to get the right package for testing. Add different test inputs to ensure the tests passes for different negative numbers and numbers greater than 1.

--- lib/grade_tool.dart
+++ lib/grade_tool.dart
@@ -1,17 +1,21 @@
 class GradeConverter {
   String changePercentToLetter(double percentGrade){
     String letter = '';
-    if(percentGrade>=0.91){
+    double gradeRoundedToHundredths = double.parse(percentGrade.toStringAsFixed(2));
+    if(gradeRoundedToHundredths>=0.91){
       letter = 'A';
-    }else if(percentGrade<=0.90 && percentGrade>0.81){
+    }else if(gradeRoundedToHundredths<=0.90 && gradeRoundedToHundredths>0.81){
       letter = 'B';
-    }else if(percentGrade<=0.80 && percentGrade>0.71){
+    }else if(gradeRoundedToHundredths<=0.80 && gradeRoundedToHundredths>0.71){
       letter = 'C';
-    }else if(percentGrade<=0.70 && percentGrade>0.61){
+    }else if(gradeRoundedToHundredths<=0.70 && gradeRoundedToHundredths>0.61){
       letter = 'D';
-    }else if(percentGrade<=0.60){
+    }else if(gradeRoundedToHundredths<=0.60 && gradeRoundedToHundredths>=0){
       letter =  'F';
+    }else{
+      letter = 'No letter applicable for grade';
     }
     return letter;
   }
+
 }
--- test/grade_tool_test.dart
+++ test/grade_tool_test.dart
@@ -1,9 +1,9 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 import 'package:grade_tool_redo/grade_tool.dart';
 
 void main(){
-  Set<String> letters = {'A','B','C','F'};
-  Set<double> percentsAsDecimals = {0.95,0.86,0.72,0.45};
+  Set<String> letters = {'A','B','C','No letter applicable for grade'};
+  Set<double> percentsAsDecimals = {1.12,0.86,0.7657687,-2.6};
   for(int i = 0;i<letters.length;i++){
     test(('${percentsAsDecimals.elementAt(i)} is a ${letters.elementAt(i)}'), (){
       final converter = GradeConverter();
