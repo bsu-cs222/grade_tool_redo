38a6a44d82e19fc88f0c5c4dde07c3246f23cc2c
Andrew Bonner <andrew.bonner@bsu.edu>
2023-11-13 14:08:55 -0500

    Change test cases to fit more possibilities. Also, change model code to better suit these cases.

--- lib/grade_tool.dart
+++ lib/grade_tool.dart
@@ -1,21 +1,35 @@
 class GradeConverter {
-  String changePercentToLetter(double percentGrade){
+  String changePercentToLetter(double percentGrade) {
     String letter = '';
-    double gradeRoundedToHundredths = double.parse(percentGrade.toStringAsFixed(2));
-    if(gradeRoundedToHundredths>=0.91){
-      letter = 'A';
-    }else if(gradeRoundedToHundredths<=0.90 && gradeRoundedToHundredths>0.81){
-      letter = 'B';
-    }else if(gradeRoundedToHundredths<=0.80 && gradeRoundedToHundredths>0.71){
-      letter = 'C';
-    }else if(gradeRoundedToHundredths<=0.70 && gradeRoundedToHundredths>0.61){
-      letter = 'D';
-    }else if(gradeRoundedToHundredths<=0.60 && gradeRoundedToHundredths>=0){
-      letter =  'F';
-    }else{
-      letter = 'No letter applicable for grade';
+    if (percentGrade >= 1.00) {
+      letter = _convert(percentGrade / 100);
+    } else {
+      letter = _convert(percentGrade);
     }
     return letter;
   }
 
+  String _convert(double percentGrade) {
+    String letter = '';
+    double gradeRoundedToHundredths =
+        double.parse(percentGrade.toStringAsFixed(2));
+    if (gradeRoundedToHundredths > 0.90) {
+      letter = 'A';
+    } else if (gradeRoundedToHundredths <= 0.90 &&
+        gradeRoundedToHundredths > 0.80) {
+      letter = 'B';
+    } else if (gradeRoundedToHundredths <= 0.80 &&
+        gradeRoundedToHundredths > 0.70) {
+      letter = 'C';
+    } else if (gradeRoundedToHundredths <= 0.70 &&
+        gradeRoundedToHundredths > 0.60) {
+      letter = 'D';
+    } else if (gradeRoundedToHundredths <= 0.60 &&
+        gradeRoundedToHundredths >= 0) {
+      letter = 'F';
+    } else {
+      letter = 'No letter applicable for grade';
+    }
+    return letter;
+  }
 }
--- test/grade_tool_test.dart
+++ test/grade_tool_test.dart
@@ -1,15 +1,35 @@
 import 'package:test/test.dart';
 import 'package:grade_tool_redo/grade_tool.dart';
 
-void main(){
-  Set<String> letters = {'A','B','C','No letter applicable for grade'};
-  Set<double> percentsAsDecimals = {1.12,0.86,0.7657687,-2.6};
-  for(int i = 0;i<letters.length;i++){
-    test(('${percentsAsDecimals.elementAt(i)} is a ${letters.elementAt(i)}'), (){
-      final converter = GradeConverter();
-      final letter = converter.changePercentToLetter(percentsAsDecimals.elementAt(i));
-      expect(letter,letters.elementAt(i));
-    });
-  }
+void main() {
+  test(('10000.00 is an A'), () {
+    final converter = GradeConverter();
+    String result = converter.changePercentToLetter(0.93);
+    expect(result, 'A');
+  });
+  test(('0.83456 is an B'), () {
+    final converter = GradeConverter();
+    String result = converter.changePercentToLetter(0.83456);
+    expect(result, 'B');
+  });
+  test(('90.67 is an A'), () {
+    final converter = GradeConverter();
+    String result = converter.changePercentToLetter(90.67);
+    expect(result, 'A');
+  });
+  test(('-5.0 is not a grade'), () {
+    final converter = GradeConverter();
+    String result = converter.changePercentToLetter(-5.1);
+    expect(result, 'No letter applicable for grade');
+  });
+  test(('70.00009 is an D'), () {
+    final converter = GradeConverter();
+    String result = converter.changePercentToLetter(70.00009);
+    expect(result, 'D');
+  });
+  test(('67 is an D'), () {
+    final converter = GradeConverter();
+    String result = converter.changePercentToLetter(67);
+    expect(result, 'D');
+  });
 }
-
